a5be8472be2b31854b49ad7e7bddac5a58d26814
PranavThorat1432 <pranavthorat95@gmail.com>
2026-09-05 11:59:37 +0530

    Ad Logout Functionality

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -22,7 +22,7 @@ export const googleAuth = async (req, res) => {
         });
 
         return res.status(200).json(user);
-
+ 
     } catch (error) {
         return res.status(500).json({
             message: `Google Auth Error: ${error}`
